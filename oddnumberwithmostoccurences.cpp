19a24a2bfb745f40df73bf0e8c8fbafd32c7db2e
yoitsmedarls <johndaryljavier26@gmail.com>
2023-11-30 20:40:43 +0800

    Improved the algorithm of the odd number activity

--- oddnumberwithmostoccurences.cpp
+++ oddnumberwithmostoccurences.cpp
@@ -16,7 +16,7 @@ int main()
 
     displayOriginalArray(sizeOfArray, numbers);
 
-    cout << "\nOdd number with the most occurences: " << findOddNumberWithMostOccurences(sizeOfArray, numbers);
+    cout << "\nNumber which occurs odd number of times: " << findOddNumberWithMostOccurences(sizeOfArray, numbers);
 
     return 0;
 }
@@ -54,99 +54,30 @@ void displayOriginalArray(int sizeOfArray, int numbers[])
     }
 }
 
-/*
-   Runs through the elements in the 'numbers' array and determines which odd number occurs the most
-   Pseudocode:
-    - Get all unique odd numbers from the input array
-    - Count the occurences of each odd number in the input array
-    - Compare the counteed occurences
-    - Output the odd number with the most occurence
-*/
 int findOddNumberWithMostOccurences(int sizeOfArray, int numbers[])
 {
-    // TODO: Optimize this function by thinking of a better algorithm that takes less time and memory
-
-    /*
-    STEP 1: Create an array containing all the unique odd numbers that are in the 'numbers' array.
-            This will be used as the test case for later, allowing us to count the occurences of each of the
-            odd numbers in the 'uniqueOddNumbers' array.
-    */
-
-    int uniqueOddNumbers[sizeOfArray] = {0};
+    int numberOfOccurences[sizeOfArray] = {0}, currentMostOccurences = 0, oddNumberWithMostOccurences = 0;
 
     for (int i = 0; i < sizeOfArray; i++)
     {
-        // Filters out the even numbers in the 'numbers' array
         if (numbers[i] % 2 == 1)
         {
-            bool isNumberRepeated = false;
-
             for (int j = 0; j < sizeOfArray; j++)
             {
-                if (numbers[i] == uniqueOddNumbers[j])
+                if (numbers[i] == numbers[j])
                 {
-                    isNumberRepeated = true;
-                    break;
+                    numberOfOccurences[i]++;
                 }
             }
-
-            // Filters out the odd numbers that are already stored in the 'uniqueOddNumbers' array;
-            if (isNumberRepeated == false)
-            {
-                uniqueOddNumbers[i] = numbers[i];
-            }
         }
     }
 
-    /*
-    RESULT OF STEP 1: An array with size equal to the size of the original array, containing all the unique
-                      odd numbers. However, there will be random zeros because of Step 1's poor design.
-
-                      Ex. Input:  {3, 2, 3, 5, 4, 7}
-                      Ex. Output: {3, 0, 0, 5, 0, 7}
-    */
-
-    /*
-    STEP 2: Creates an array storing the number of occurence of the odd numbers in the same index.
-    */
-
-    int occurencesPerNumber[sizeof(uniqueOddNumbers) / 4] = {0};
-
-    for (int i = 0; i < sizeof(uniqueOddNumbers) / 4; i++)
+    for (int i = 0; i < sizeOfArray; i++)
     {
-        // Increments the value at the index corresponding to the unique odd number for every occurence
-        for (int j = 0; j < sizeOfArray; j++)
+        if (numberOfOccurences[i] > currentMostOccurences)
         {
-            if (uniqueOddNumbers[i] == numbers[j])
-            {
-                occurencesPerNumber[i]++;
-            }
-        }
-    }
-
-    /*
-    RESULT OF STEP 2: An array with size equal to the size of the 'uniqueOddNumbers' array, containing the number
-                      of occurence of each odd number. There will be random zeros once again.
-
-                      Ex. Input: {3, 0, 0, 5, 0, 7}
-
-                      If 3 is repeated 2 times, 5 is repeated 4 times, and 7 is repeated 1 time;
-
-                      Ex. Output: {2, 0, 0, 4, 0, 1}.
-    */
-
-    /*
-    STEP 3: Compares the values in the 'occurencesPerNumber' array and outputs the odd number with the most occurences
-    */
-
-    int oddNumberWithMostOccurences = 0, currentMostOccurences = 0;
-
-    for (int i = 0; i < sizeof(occurencesPerNumber) / 4; i++)
-    {
-        if (occurencesPerNumber[i] > currentMostOccurences)
-        {
-            currentMostOccurences = occurencesPerNumber[i];
-            oddNumberWithMostOccurences = uniqueOddNumbers[i];
+            currentMostOccurences = numberOfOccurences[i];
+            oddNumberWithMostOccurences = numbers[i];
         }
     }
 
